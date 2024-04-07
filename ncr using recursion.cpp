85c7252eeea0f1e42450a3553cf60b950cb2ec8a
Sanchit Pawa <sanchitpawa1706@gmail.com>
2024-04-07 14:30:14 +0530

    deleted:    tut13.cpp 	modified:   tut13.exe

---
+++ ncr using recursion.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<stdio.h>
+using namespace std;
+int ncr(int t,int p){
+    if(p==0||p==t){
+        return 1;
+    }
+    else{
+        return ncr(t-1,p-1)+ncr(t-1,p);
+    }
+}
+int main(){
+    cout<<ncr(3,2);
+    return 0;
+}
